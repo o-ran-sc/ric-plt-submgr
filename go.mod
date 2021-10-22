f0aeaf42668ad908129b41df971148f0e7738282
Anssi Mannila <anssi.mannila@nokia.com>
2021-10-22 09:52:02 +0300

    E2 restart handling added - Implementation is not yet complete - Connection break during signaling not yet implemented - New test cases are needed
    Change-Id: I7b5c52826910a4dcafff8938d414d7e9f57fa2ed
    Signed-off-by: Anssi Mannila <anssi.mannila@nokia.com>

--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ replace gerrit.o-ran-sc.org/r/ric-plt/e2ap => ./e2ap/
 require (
 	gerrit.o-ran-sc.org/r/ric-plt/e2ap v0.0.0-00010101000000-000000000000
 	gerrit.o-ran-sc.org/r/ric-plt/sdlgo v0.8.0
+	gerrit.o-ran-sc.org/r/ric-plt/nodeb-rnib.git/entities v1.2.1 // indirect
+	gerrit.o-ran-sc.org/r/ric-plt/sdlgo v0.8.0
 	gerrit.o-ran-sc.org/r/ric-plt/xapp-frame v0.0.0-00010101000000-000000000000
 	github.com/go-openapi/errors v0.19.3
 	github.com/go-openapi/runtime v0.19.4
---
+++ pkg/control/e2if_state_test.go
@@ -0,0 +1,245 @@
+/*
+==================================================================================
+  Copyright (c) 2019 AT&T Intellectual Property.
+  Copyright (c) 2019 Nokia
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================================
+*/
+
+package control
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	//"testing"
+	"gerrit.o-ran-sc.org/r/ric-plt/nodeb-rnib.git/entities"
+	"gerrit.o-ran-sc.org/r/ric-plt/xapp-frame/pkg/xapp"
+)
+
+var xappRnibMock *XappRnibMock
+
+type XappRnibMock struct {
+	Mutex            sync.Mutex
+	nbIdentityMap    map[string]xapp.RNIBNbIdentity
+	RNIBNodebInfoMap map[string]xapp.RNIBNodebInfo
+	RnibSubscription RnibSubscription // Submgr can have only one subscription
+}
+
+type RnibSubscription struct {
+	Channel string                            // Subscribed channel/topic "RAN_CONNECTION_STATUS_CHANGE"
+	cb      func(ch string, events ...string) // Submgr's call back function
+}
+
+func CreateXappRnibIfMock() *XappRnibMock {
+	fmt.Println("XappRnibMock: CreateXappRnibIfMock()")
+	xappRnibMock = new(XappRnibMock)
+	xappRnibMock.Init()
+	return xappRnibMock
+}
+
+func (x *XappRnibMock) Init() {
+	x.nbIdentityMap = make(map[string]xapp.RNIBNbIdentity, 0)
+	x.RNIBNodebInfoMap = make(map[string]xapp.RNIBNodebInfo, 0)
+
+	// Current UT test cases use these ran names
+	x.CreateGnb("RAN_NAME_1", entities.ConnectionStatus_CONNECTED)
+	x.CreateGnb("RAN_NAME_11", entities.ConnectionStatus_CONNECTED)
+	x.CreateGnb("RAN_NAME_2", entities.ConnectionStatus_CONNECTED)
+
+	x.CreateGnb("gnb_208_092_303030", entities.ConnectionStatus_CONNECTED) // This same value is used in gnb simulator!
+	x.CreateGnb("gnb_369_11105_aaaaa8", entities.ConnectionStatus_DISCONNECTED)
+
+}
+
+/*
+func TestSomething(t *testing.T) {
+
+	x.CreateGnb("gnb_208_092_303030", entities.ConnectionStatus_CONNECTED)   	// This same value is used in gnb simulator!
+	x.CreateGnb("gnb_369_11105_aaaaa8", entities.ConnectionStatus_DISCONNECTED)
+	x.CreateGnb("gnb_369_11105_aaaaa7", ConnectionStatus_UNKNOWN_CONNECTION_STATUS)
+	x.CreateGnb("gnb_369_11105_aaaaa6", onnectionStatus_CONNECTED_SETUP_FAILED)
+	x.CreateGnb("gnb_369_11105_aaaaa5", ConnectionStatus_CONNECTING)
+	x.CreateGnb("gnb_369_11105_aaaaa4", ConnectionStatus_SHUTTING_DOWN)
+	x.CreateGnb("gnb_369_11105_aaaaa3", ConnectionStatus_SHUT_DOWN)
+
+	//x.CreateGnb("gnb_208_092_303030", entities.ConnectionStatus_DISCONNECTED)   // DISCONNECTED.
+
+	mainCtrl.c.e2IfState.ReadE2ConfigurationFromRnib()
+	mainCtrl.c.e2IfState.SubscribeChannels()
+	if err := mainCtrl.c.e2IfStateDb.XappRnibStoreAndPublish("RAN_CONNECTION_STATUS_CHANGE", "RAN_NAME_1_CONNECTED", "key1", "data1"); err != nil {
+		t.Errorf("XappRnibStoreAndPublish failed: %v", err)
+	}
+
+	if err := mainCtrl.c.e2IfStateDb.XappRnibStoreAndPublish("RAN_CONNECTION_STATUS_CHANGE", "RAN_NAME_11_CONNECTED", "key1", "data1"); err != nil {
+		t.Errorf("XappRnibStoreAndPublish failed: %v", err)
+	}
+
+	if err := mainCtrl.c.e2IfStateDb.XappRnibStoreAndPublish("RAN_CONNECTION_STATUS_CHANGE", "RAN_NAME_2_CONNECTED", "key1", "data1"); err != nil {
+		t.Errorf("XappRnibStoreAndPublish failed: %v", err)
+	}
+	time.Sleep(time.Duration(2) * time.Second)
+}
+*/
+
+func (x *XappRnibMock) CreateGnb(gnbId string, connectionStatus entities.ConnectionStatus) {
+
+	xapp.Logger.Debug("XappRnibMock: CreateGnb() gnbId=%v, ConnectionStatus=%v", gnbId, connectionStatus)
+	nb := xapp.RNIBNodebInfo{}
+	nb.NodeType = xapp.RNIBNodeGNB
+	nb.ConnectionStatus = connectionStatus
+	if nb.ConnectionStatus < 0 || nb.ConnectionStatus > 6 {
+		xapp.Logger.Error("XappRnibMock: CreateGnb() Incorrect connectionStatus=%v", nb.ConnectionStatus)
+		return
+	}
+	nb.Ip = "localhost"
+	nb.Port = 5656
+	gnb := xapp.RNIBGnb{}
+
+	gnb.ServedNrCells = nil
+	nb.Configuration = &xapp.RNIBNodebInfoGnb{Gnb: &gnb}
+	nbIdentity := &xapp.RNIBNbIdentity{
+		InventoryName: gnbId,
+		GlobalNbId: &xapp.RNIBGlobalNbId{
+			PlmnId: "001EF5",
+			NbId:   "0045FE50",
+		},
+	}
+
+	err := x.XappRnibSaveNodeb(nbIdentity, &nb)
+	if err != nil {
+		xapp.Logger.Error("XappRnibMock: XappRnibSaveNodeb() failed. Error: %v", err)
+	}
+}
+
+func (x *XappRnibMock) XappRnibSaveNodeb(nbIdentity *xapp.RNIBNbIdentity, nodeb *xapp.RNIBNodebInfo) xapp.RNIBIRNibError {
+
+	xapp.Logger.Debug("XappRnibMock: XappRnibSaveNodeb() inventoryName=%v, ConnectionStatus=%v", nbIdentity.InventoryName, nodeb.ConnectionStatus)
+	x.Mutex.Lock()
+	defer x.Mutex.Unlock()
+	x.nbIdentityMap[nbIdentity.InventoryName] = *nbIdentity
+	x.RNIBNodebInfoMap[nbIdentity.InventoryName] = *nodeb
+	return nil
+}
+
+func (x *XappRnibMock) XappRnibGetNodeb(inventoryName string) (*xapp.RNIBNodebInfo, xapp.RNIBIRNibError) {
+
+	x.Mutex.Lock()
+	defer x.Mutex.Unlock()
+	xapp.Logger.Debug("XappRnibMock: XappRnibGetNodeb() inventoryName=%v", inventoryName)
+	nodebInfo, ok := x.RNIBNodebInfoMap[inventoryName]
+	if ok {
+		return &nodebInfo, nil
+	} else {
+		return nil, fmt.Errorf("XappRnibMock: XappRnibGetNodeb() failed: inventoryName=%s:", inventoryName)
+	}
+}
+
+func (x *XappRnibMock) XappRnibSubscribe(cb func(string, ...string), channel string) error {
+
+	if x.RnibSubscription.Channel == "RAN_CONNECTION_STATUS_CHANGE" {
+		xapp.Logger.Debug("XappRnibMock: RAN_CONNECTION_STATUS_CHANGE channel already subscribed")
+		return nil
+	}
+	if x.RnibSubscription.Channel == "" {
+		x.RnibSubscription.cb = cb
+		x.RnibSubscription.Channel = channel
+		xapp.Logger.Debug("XappRnibMock: RAN_CONNECTION_STATUS_CHANGE subscribed")
+		return nil
+	} else {
+		return fmt.Errorf("XappRnibMock: Invalid channel/topic to subscribe: channel = %s", channel)
+	}
+}
+
+func (x *XappRnibMock) XappRnibGetListGnbIds() ([]*xapp.RNIBNbIdentity, xapp.RNIBIRNibError) {
+
+	xapp.Logger.Debug("XappRnibMock: XappRnibGetListGnbIds()")
+	x.Mutex.Lock()
+	defer x.Mutex.Unlock()
+	var nbIdentities []*xapp.RNIBNbIdentity
+	for _, nbIdentity := range x.nbIdentityMap {
+		nbIdentities = append(nbIdentities, &nbIdentity)
+	}
+	xapp.Logger.Debug("XappRnibMock: XappRnibGetListGnbIds(). len(nbIdentities) = %v", len(nbIdentities))
+	return nbIdentities, nil
+}
+
+func (x *XappRnibMock) XappRnibStoreAndPublish(channel string, event string, pairs ...interface{}) error {
+
+	x.Mutex.Lock()
+	defer x.Mutex.Unlock()
+	xapp.Logger.Debug("XappRnibMock: Change published. channel=%s, event=%s", channel, event)
+	if channel != "RAN_CONNECTION_STATUS_CHANGE" || channel == "" || event == "" {
+		xapp.Logger.Debug("XappRnibMock: Invalid change published. channel=%s, event=%s", channel, event)
+	}
+
+	nbId, connectionStatus, err := ExtratNbIdAndConnectionStatus(event)
+	if err != nil {
+		xapp.Logger.Error("XappRnibMock: ExtratNbIdAndConnectionStatus. Err=%s", err)
+	}
+
+	nbIdentity, ok := x.nbIdentityMap[nbId]
+	if ok {
+		nbIdentity.ConnectionStatus = connectionStatus
+	}
+
+	if x.RnibSubscription.cb != nil {
+		x.RnibSubscription.cb(channel, event)
+	} else {
+		xapp.Logger.Error("XappRnibMock: x.RnibSubscription.cb == nil")
+	}
+	return nil
+}
+
+func ExtratNbIdAndConnectionStatus(s string) (string, entities.ConnectionStatus, error) {
+
+	var connectionStatus entities.ConnectionStatus
+	var nbId string
+	if strings.Contains(s, "_UNKNOWN_CONNECTION_STATUS") {
+		connectionStatus = entities.ConnectionStatus_UNKNOWN_CONNECTION_STATUS
+		splitStringTbl := strings.Split(s, "_UNKNOWN_CONNECTION_STATUS")
+		nbId = splitStringTbl[0]
+	} else if strings.Contains(s, "_CONNECTED") {
+		connectionStatus = entities.ConnectionStatus_CONNECTED
+		splitStringTbl := strings.Split(s, "_CONNECTED")
+		nbId = splitStringTbl[0]
+	} else if strings.Contains(s, "_DISCONNECTED") {
+		connectionStatus = entities.ConnectionStatus_DISCONNECTED
+		splitStringTbl := strings.Split(s, "_DISCONNECTED")
+		nbId = splitStringTbl[0]
+	} else if strings.Contains(s, "_CONNECTED_SETUP_FAILED") {
+		connectionStatus = entities.ConnectionStatus_CONNECTED_SETUP_FAILED
+		splitStringTbl := strings.Split(s, "_CONNECTED_SETUP_FAILED")
+		nbId = splitStringTbl[0]
+	} else if strings.Contains(s, "_CONNECTING") {
+		connectionStatus = entities.ConnectionStatus_CONNECTING
+		splitStringTbl := strings.Split(s, "_CONNECTING")
+		nbId = splitStringTbl[0]
+	} else if strings.Contains(s, "_SHUTTING_DOWN") {
+		connectionStatus = entities.ConnectionStatus_SHUTTING_DOWN
+		splitStringTbl := strings.Split(s, "_SHUTTING_DOWN")
+		nbId = splitStringTbl[0]
+	} else if strings.Contains(s, "_SHUT_DOWN") {
+		connectionStatus = entities.ConnectionStatus_SHUT_DOWN
+		splitStringTbl := strings.Split(s, "_SHUT_DOWN")
+		nbId = splitStringTbl[0]
+	} else {
+		return "", 0, fmt.Errorf("XappRnibMock: Invalid connection status. %s", s)
+	}
+	if len(nbId) == 0 {
+		return "", 0, fmt.Errorf("ExtractNbiIdFromString(): len(nbId) == 0 ")
+	}
+	return nbId, connectionStatus, nil
+}
